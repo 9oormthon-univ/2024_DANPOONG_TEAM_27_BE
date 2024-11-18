6508bf72ff0497ba9a897a89ba8c23669810e4d4
Junyeong-An <2117ab@gmail.com>
2024-11-18 20:02:19 +0900

    chore: 프로젝트 기본 셋팅

---
+++ src/main/java/com/luckit/LuckitApplication.java
@@ -0,0 +1,13 @@
+package com.luckit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckitApplication.class, args);
+    }
+
+}
