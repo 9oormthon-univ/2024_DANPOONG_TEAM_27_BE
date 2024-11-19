9cc2652f12039de9b2c57f988199a74936692d1e
Junyeong-An <2117ab@gmail.com>
2024-11-19 16:21:46 +0900

    feat: 유저 도메인 추가

---
+++ src/main/java/com/luckit/user/domain/User.java
@@ -0,0 +1,40 @@
+package com.luckit.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String nickname;
+
+    private String email;
+
+    private String profileImage;
+
+    private String gender;
+
+    private LocalDateTime date_of_birth;
+
+    private RoleType roleType;
+
+
+
+
+}
