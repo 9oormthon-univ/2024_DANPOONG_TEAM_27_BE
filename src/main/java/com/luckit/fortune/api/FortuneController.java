a8af626469ed3dc7f1ca9e950edf3984ff978d51
Junyeong-An <2117ab@gmail.com>
2024-11-22 23:14:40 +0900

    refactor: 스웨거 설명 추가

--- src/main/java/com/luckit/fortune/api/FortuneController.java
+++ src/main/java/com/luckit/fortune/api/FortuneController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping("/api/v1/fortune")
-@Tag(name = "운세 생성", description = "운세 생성을 담당하는 api 그룹")
+@Tag(name = "운세 생성", description = "운세 생성을 담당하는 API 그룹")
 public class FortuneController {
 
     private final FortuneService fortuneService;
@@ -31,7 +31,7 @@ public class FortuneController {
             summary = "유저의 온보딩 목표를 생성",
             description = "회원가입한 유저의 온보딩 목표를 생성합니다.",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "목표 생성을 성공했습니다.")
+                    @ApiResponse(responseCode = "200", description = "온보딩 목표 생성을 성공했습니다.")
             }
     )
     public ResponseEntity<ApiResponseTemplate<List<GoalPeriod>>> getOnboardingFortune(Principal principal) {
@@ -48,15 +48,43 @@ public class FortuneController {
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "운세 생성을 성공했습니다.",
-                            content = @Content(schema = @Schema(example = "{ \"status\": 200, \"message\": \"사주 정보 조회에 성공했습니다\", \"data\": { \"fortuneKeywords\": [\"새로운 시작\", \"에너지\", \"호기심\", \"성장\"], \"shortFortune\": \"새로운 시작을 받아들이세요.\", \"fullFortune\": \"오늘은 새로운 기회와 경험을 받아들이기에 완벽한 날입니다. 호기심과 낙관적인 태도로 도전에 임할 수 있는 에너지가 넘치는 날입니다. 열린 마음을 유지하면 예상치 못한 영역에서 성장을 발견할 수 있습니다.\", \"categoryFortuneScores\": { \"Money\": 75, \"Study\": 90, \"Health\": 85, \"Love\": 82, \"Work\": 88 }, \"timeOfDayFortuneScores\": { \"Morning\": 83 }, \"overallFortuneScore\": 83 } }"))
+                            description = "일일 운세 생성을 성공했습니다.",
+                            content = @Content(
+                                    schema = @Schema(
+                                            example = """
+                                                    {
+                                                        "status": 200,
+                                                        "message": "사주 정보 조회에 성공했습니다",
+                                                        "data": {
+                                                            "fortuneKeywords": ["새로운 시작", "에너지", "기회", "초점"],
+                                                            "shortFortune": "새로운 길을 받아들이세요.",
+                                                            "fullFortune": "오늘은 새로운 시작입니다. 열정을 가지고 새로운 기회를 맞이하세요. 최적의 생산성을 위해 에너지의 균형을 맞추고 당면한 업무에 집중하세요. 자신의 능력을 믿고 긍정적인 시각을 유지하세요.",
+                                                            "categoryFortuneScores": {
+                                                                "Money": 80,
+                                                                "Study": 78,
+                                                                "Health": 82,
+                                                                "Love": 75,
+                                                                "Work": 85
+                                                            },
+                                                            "timeOfDayFortuneScores": {
+                                                                "Morning": 80,
+                                                                "Afternoon": 83,
+                                                                "Night": 81,
+                                                                "Average": 81
+                                                            },
+                                                            "overallFortuneScore": 81
+                                                        }
+                                                    }
+                                                    """
+                                    )
+                            )
                     )
             }
     )
-public ResponseEntity<ApiResponseTemplate<UserFortuneResponseDto>> createDailyFortune(Principal principal) {
+    public ResponseEntity<ApiResponseTemplate<UserFortuneResponseDto>> createDailyFortune(Principal principal) {
 
-    ApiResponseTemplate<UserFortuneResponseDto> data = fortuneService.createDailyFortune(principal);
+        ApiResponseTemplate<UserFortuneResponseDto> data = fortuneService.createDailyFortune(principal);
 
-    return ResponseEntity.status(data.getStatus()).body(data);
-}
+        return ResponseEntity.status(data.getStatus()).body(data);
+    }
 }
